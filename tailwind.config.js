e047a6ac1a1e0eaf9a2c8c2202736afa6ef35d53
Liz Wilson <lizriderwilson@gmail.com>
2021-04-07 13:19:14 -0400

    Change default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Nintendo Switch UI', 'sans-serif']
+    },
   },
   variants: {},
   plugins: [],
